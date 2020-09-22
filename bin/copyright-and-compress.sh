8cde9262f8086b98035b64ff849264b64ec23adb
Gaelen Hadlett <gaelenh@gmail.com>
2020-09-21 22:59:01 -0400

    Updated image compress and copyright script

--- bin/copyright-and-compress.sh
+++ bin/copyright-and-compress.sh
@@ -1,1 +1,2 @@
-/usr/local/opt/mozjpeg/bin/cjpeg -quality 75 -targa -outfile $OF.jpg $IF.tga exiv2 -M"set Exif.Image.Copyright © 2015 Sunset Spark, Inc." *.jpg
+/usr/local/opt/mozjpeg/bin/cjpeg -quality 75 -targa -outfile $OF.jpg $IF.tga
+exiv2 -M"set Exif.Image.Copyright © 2020 Sunset Spark, Inc." *.jpg
